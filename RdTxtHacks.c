f53436c624e8ef0a9e7c4a37e4acc84e34d1a7a3
awsdert <gb2985@gmail.com>
2014-08-01 09:05:06 +0100

    Fixed Hack read
    Hack names will now be read properly and switch to reading data straight
    after until either a empty line or the " is encountered, presently
    unable to determine a way to deal with situations like below where
    neither an empty line or the " character is availble:
    Hack1
    Hack1Data
    Hack2
    Hack2Data
    Hack3
    Hack3Data

--- RdTxtHacks.c
+++ RdTxtHacks.c
@@ -1,13 +1,14 @@
 #include "guiMain.h"
 long
-_RdTxtHacks( HACK_FUNC *hfunc, HACKS *hacks, CODE_FUNC *cfunc, CODES *codes, Ipipe *file, CODELIST *cl, long hid, long pid, long cdepth, long  mode )
+_RdTxtHacks( HACK_FUNC *hfunc, HACKS *hacks, CODE_FUNC *cfunc, CODES *codes, Ipipe *file, CODELIST *cl, long cgid, long hid, long pid, long cdepth, long  mode, usv i )
 {
   DWORD len = 0;
-  usv i = 0;
-  long cid = 0, depth = 0, gid = 0, j = 0, k = 0;
+  long cid = 0, depth = cdepth, gid = -1, j = 0, k = 0;
   HACK *hack = &hacks->buff[hid];
-  if ( !mode )
+  if ( mode == 0 )
     len = ipRdLine( file, cl->x[i], cl->cols );
+  else
+    len = strlen( cl->x[i] );
   for
   (
     ; len; memset( cl->x[i], 0, cl->cols ),
@@ -15,7 +16,7 @@ _RdTxtHacks( HACK_FUNC *hfunc, HACKS *hacks, CODE_FUNC *cfunc, CODES *codes, Ipi
   )
   {
     if ( cl->x[i][0] == '"' )
-      mode = ( mode == 1 ) ? 2 : 0;
+      mode = ( mode == 1 || mode == 2 ) ? 2 : 0;
     else if ( cl->x[i][0] == '\n' || cl->x[i][0] == '\r' )
     {
       mode = ( mode == 1 ) ? 2 : 0;
@@ -30,41 +31,64 @@ _RdTxtHacks( HACK_FUNC *hfunc, HACKS *hacks, CODE_FUNC *cfunc, CODES *codes, Ipi
       cl->rows = 30;
       if ( hid == 0 )
       {
-        _RdTxtHacks( hfunc, hacks, cfunc, codes, file, cl, 1, 0, 0, 2 );
+        _RdTxtHacks( hfunc, hacks, cfunc, codes, file, cl, gid, 1, 0, 0, 2, i );
         break;
       }
-      else if ( depth == cdepth )
-      {
+      else if ( hack->pid != pid || depth == cdepth )
         ++hid;
-        hack->pid = pid;
-      }
       else if ( depth < cdepth )
       {
-        if ( depth >= 0 )
+        if ( depth > 0 )
           hack->pid = -depth;
+        else if ( cdepth > 0 )
+          hack->pid = -(cdepth - 1);
+        else
+          hack->pid = 0;
         break;
       }
       else
       {
         hack->pid = hid - 1;
-        hid = _RdTxtHacks( hfunc, hacks, cfunc, codes, file, cl, hid + 1, hid - 1, depth, 2 );
+        hid = _RdTxtHacks( hfunc, hacks, cfunc, codes, file, cl, cgid, hid + 1, hid - 1, depth, 2, i );
+        mode = 3;
       }
       /* Finish reading hack name/header */
       hack = &hacks->buff[ hid ];
-      if ( hack->pid < 0 )
+      if ( mode == 3 )
       {
-        if ( -hack->pid != cdepth )
-          break;
-        hack->pid = pid;
+        if ( hack->pid < 0 )
+        {
+          if ( -hack->pid != cdepth )
+            break;
+          hack->pid = pid;
+        }
+        ++hid;
+        hack = &hacks->buff[ hid ];
       }
       mode = 0;
     }
     /* Retrieve hack information */
-    if ( !mode )
+    if ( mode == 0 )
     {
+      /*
+        gid is only for converting back to txt,
+        also for weeding out invalid hacks.
+        Example for gid not supported:
+          return -1 for valid hack,
+          return -2 for invalid hack
+        Example for gid being supported
+          Hacks contain their own gid which is compared against (M) gid
+        0 and up are treated as valid IDs by converters so -1 is a good
+        way to ensure the user is forced to enter a gid before converters
+        requiring one can work
+      */
+      hack->pid = pid;
       gid = hfunc->txt2raw( hack, file );
-      if ( hack->pid < 0 )
-        break;
+      if ( gid != cgid && hid > 0 )
+      {
+        mode = 3;
+        continue;
+      }
       j = 0;
       k = (cl->x[i][0] == '"') ? 1 : 0;
       depth = 0;
@@ -82,17 +106,18 @@ _RdTxtHacks( HACK_FUNC *hfunc, HACKS *hacks, CODE_FUNC *cfunc, CODES *codes, Ipi
       }
       while ( k < cl->cols && cl->x[i][k] );
       hack->name[ NAME_LAST ] = 0;
-      IupMessage( "Hack Load", hacks->buff[i].name );
+      //IupMessage( "Hack Load", hacks->buff[i].name );
       mode = 1;
       i = 0;
     }
     /* Retrieve codelist */
-    else
+    else if ( mode == 1 )
     {
       ++i;
       if ( i == cl->rows )
         break;
     }
+    else continue;
   }
   return hid;
 }
@@ -102,5 +127,5 @@ RdTxtHacks( HACK_FUNC *hfunc, HACKS *hacks, CODE_FUNC *cfunc, CODES *codes, Ipip
   CODELIST cl = {0};
   cl.rows = 30;
   cl.cols = 50;
-  _RdTxtHacks( hfunc, hacks, cfunc, codes, file, &cl, 0, 0, 0, 0 );
+  _RdTxtHacks( hfunc, hacks, cfunc, codes, file, &cl, -1, 0, 0, 0, 0, 0 );
 }
